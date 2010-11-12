e9493aed87d6bbaf44a368c627b874432812d8aa
peterchappell <peter@experiencebureau.com>
2010-11-12 11:20:43 +1000

    Got images working.

--- node-product.tpl.php
+++ node-product.tpl.php
@@ -1,44 +1,29 @@
 <?php
-// Grabs the firsts image path and sets $imagePath.
-$imagePath = $node->field_image_cache['0']['filepath'];
+// Grab the firsts image path and sets $imagePath.
+if ( $user->uid ) {
+    // user logged in
+    $image = $node->field_image_cache['0'];
+    $lightBoxImagePath = "/sites/default/files/imagecache/product_full/";
+}
+else {
+    // Not logged in
+    $image = $node->field_image_preview['0'];
+    $lightBoxImagePath = "/sites/default/files/imagecache/product_full/";
+}
 ?>
 
 <div class="view view-uc-shoe-single view-id-uc_shoe_single view-display-id-page_1 view-dom-id-1">
 <div class="view-content">
 <div class="views-row views-row-1 views-row-odd views-row-first views-row-last">
 
-<!--    <div id="image"><img src="/<?php /*print $imagePath; */?>" alt="Title"></div>-->
-
-<?php // get all images
-foreach ($node->field_image_cache as $images) {
-?>
+    <?php // Product image ?>
     <div class="views-field-field-image-preview-fid">
     <div class="field-content">
-    <a class="thickbox" href="/sites/default/files/imagecache/product/<?php print $images['filename']?>">
-        <!-- <img src="/files/imagecache/uc_thumbnail/<?php /*print $images['filename']; */?>" alt="<?php /*print $images['alt']; */?>" hspace="0" border="0" class="styleimages">-->
-        <?php print theme('imagecache', 'product', $images['filepath'], $images['alt'], $images['title']); ?>
-    </a>
+        <a rel="lightbox[<?php print $nid ?>][<?php print drupal_get_title() ?>]" href="<?php print $lightBoxImagePath . $image['filepath'] ?>">
+        <?php print theme('imagecache', 'product', $image['filepath'], $image['alt'], $image['title']); ?>
+        </a>
     </div>
     </div>
-<?php
-}
-?>
-   
-<!--    <div class="views-field-field-image-cache-fid">
-        <div class="field-content">
-            <a class="imagefield imagefield-lightbox2 imagefield-lightbox2-product imagefield-field_image_cache imagecache imagecache-field_image_cache imagecache-product imagecache-field_image_cache-product lightbox-processed" rel="lightbox[24][click to enlarge]" href="http://austere.local/sites/default/files/imagecache/product_full/sz39c-9cm_0.jpg">
-                <img src="/files/imagecache/product/<?php /*print $imagePath; */?>" alt="Title">
-            </a>
-        </div>
-    </div>-->
-
-<!--    <div class="views-field-field-image-preview-fid">
-        <span class="field-content">
-            <a class="imagefield imagefield-lightbox2 imagefield-lightbox2-product imagefield-field_image_preview imagecache imagecache-field_image_preview imagecache-product imagecache-field_image_preview-product lightbox-processed" rel="lightbox[24][click to enlarge]" href="http://austere.local/sites/default/files/imagecache/product_full/sz39c-9cm-public.jpg">
-                <img height="180" width="180" title="click to enlarge" alt="the shoe" src="http://austere.local/sites/default/files/imagecache/product/sz39c-9cm-public.jpg">
-            </a>
-        </span>
-    </div>-->
 
     <div class="views-field-tid">
         <label class="views-label-tid">Specifications:</label>
