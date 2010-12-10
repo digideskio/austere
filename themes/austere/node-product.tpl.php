73d31038c64a97b80c437cb2fed52cf4c471299a
peterchappell <peter@experiencebureau.com>
2010-12-10 12:26:32 +1000

    moved repo to root of sites/all

---
+++ themes/austere/node-product.tpl.php
@@ -0,0 +1,70 @@
+<?php
+// Grab the firsts image path and sets $imagePath.
+if ( $user->uid ) {
+    // user logged in
+    $image = $node->field_image_cache['0'];
+    $lightBoxImagePath = "/shop/sites/default/files/imagecache/product_full/" . $image['filename'];
+}
+else {
+    // Not logged in
+    $image = $node->field_image_preview['0'];
+    $lightBoxImagePath = "/shop/" . $image['filepath'];
+}
+?>
+
+<div class="view view-uc-shoe-single view-id-uc_shoe_single view-display-id-page_1 view-dom-id-1">
+<div class="view-content">
+<div class="views-row views-row-1 views-row-odd views-row-first views-row-last">
+
+    <?php // Product image ?>
+    <div class="views-field-field-image-preview-fid">
+    <div class="field-content">
+        <a rel="lightbox[<?php print $nid ?>][<?php print drupal_get_title() ?>]" href="<?php print $lightBoxImagePath ?>">
+        <?php print theme('imagecache', 'product', $image['filepath'], $image['alt'], $image['title']); ?>
+        </a>
+    </div>
+    </div>
+
+    <div class="views-field-tid">
+        <label class="views-label-tid">Specifications:</label>
+            <div class="field-content">
+            <div class="item-list">
+                <ul>
+                    <li class="first"><b class="key">Size</b><span class="value"><?php print $node_vocabulary[1] ?></span></li>
+                    <li><b class="key">Heel Height</b><span class="value"><?php print $node_vocabulary[2] ?></span></li>
+                    <li><b class="key">Toe Box</b><span class="value"><?php print $node_vocabulary[4] ?></span></li>
+                    <li><b class="key">Heel Cage</b><span class="value"><?php print $node_vocabulary[3] ?></span></li>
+                    <li><b class="key">Sole</b><span class="value"><?php print $node_vocabulary[7] ?></span></li>
+                    <li><b class="key">Upper</b><span class="value"><?php print $node_vocabulary[8] ?></span></li>
+                    <li class="last"><b class="key">Straps</b><span class="value"><?php print $node_vocabulary[6] ?></span></li>
+                </ul>
+            </div>
+            </div>
+    </div>
+
+    <div class="views-field-body">
+        <div class="field-content">
+            <ul>
+                <?php /*if (!empty($node->content['body']['#value']) && strcmp($node->content['body']['#value'],'<div class="product-body"></div>')!=0 ) { */?><!--
+                <li><b class="key">description</b><div class="value"><?php /*print $node->content['body']['#value'] */?></div></li>
+                --><?php /*} */?>
+                <li><b class="key">price</b><span class="value"><?php print uc_currency_format($node->sell_price); ?></span></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="views-field-buyitnowbutton">
+        <div class="field-content">
+            <ul>
+                <li>
+                    <b class="key"><?php print $node->model ?></b>
+                    <div class="value" id="cartButton">
+                        <?php print $node->content['add_to_cart']["#value"]; ?>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
+</div>
---
+++ themes/austere/page.tpl.php
@@ -0,0 +1,96 @@
+<?php // $Id: page.tpl.php,v 1.1.2.5.2.14.2.12 2010/03/01 13:37:46 psynaptic Exp $ ?>
+<?php $nodeId = $node->nid ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html<?php print drupal_attributes($html_attr); ?>>
+
+<head>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <!--[if lt IE 8]><link type="text/css" rel="stylesheet" media="all" href="/shop/sites/all/themes/clean/css/ie-lt8.css" /><![endif]-->
+  <!--[if lte IE 6]><link type="text/css" rel="stylesheet" media="all" href="/shop/sites/all/themes/austere/ie-lte6.css" /><![endif]-->
+    <?php
+    if ($_GET["format"] == "lightbox2") {
+      print "<link type='text/css' rel='stylesheet' media='all' href='/shop/sites/all/themes/austere/lightbox.css' />";
+    }
+    ?>
+  <title><?php print $head_title; ?></title>
+</head>
+
+<body<?php print drupal_attributes($attr); ?>>
+<div id="container">
+
+  <div id="fixedHeader">
+      <div id="branding">
+        <div class="limiter clear-block">
+          <?php print $logo_themed; ?>
+          <?php print $site_name_themed; ?>
+          <?php print $site_slogan_themed; ?>
+          <?php print $mission_themed; ?>
+          <?php print $search_box; ?>
+        </div>
+      </div>
+      <div id="navigation">
+        <div class="limiter clear-block">
+          <?php print $skip_link; ?>
+          <?php print $primary_links; ?>
+          <?php print $secondary_links; ?>
+        </div>
+      </div>
+      <?php if ($header): ?>
+        <div id="header">
+            <div class="limiter clear-block">
+                <?php print $header; ?>
+            </div>
+        </div>
+      <?php endif; ?>
+  </div>
+
+  <div id="page">
+    <div class="limiter clear-block">
+      <div id="main" class="clear-block">
+
+        <?php if ($left): ?>
+          <div id="left" class="sidebar">
+            <?php if(! $user->uid == 0) { ?>
+                <p id="loggedInAs">Logged in as: <a href="/shop/user"><?php print $user->name ?></a></p>
+            <?php } ?>
+            <?php print $left; ?>
+          </div>
+        <?php endif; ?>
+
+        <div id="content" class="clear-block">
+          <?php print $tabs; ?>
+          <?php print $messages; ?>
+          <?php print $help; ?>
+
+          <?php print $breadcrumb; ?>
+          <?php if ($title && $nodeId!=22 && $nodeId!=7 && $nodeId!=8): ?>
+            <h1 class="page-title"><?php print $title; ?></h1>
+          <?php endif; ?>
+
+          <?php print $content; ?>
+        </div>
+
+        <?php if ($right): ?>
+          <div id="right" class="sidebar">
+            <?php print $right; ?>
+          </div>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+
+  <div id="footer">
+    <div class="limiter clear-block">
+      <?php print $feed_icons; ?>
+      <?php print $footer; ?>
+      <?php print $footer_message; ?>
+    </div>
+  </div>
+
+  <?php print $closure; ?>
+</div>
+</body>
+</html>
